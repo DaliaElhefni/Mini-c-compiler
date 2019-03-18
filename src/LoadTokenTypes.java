479295c21ae5225d61eb65da579c4c1dbc767597
daliaalaa <daliaelhefnii@gmail.com>
2019-03-18 21:21:13 +0200

    Add LoadTokenTypes Class

---
+++ src/LoadTokenTypes.java
@@ -0,0 +1,10 @@
+import java.util.HashMap;
+
+public class LoadTokenTypes {
+    public static HashMap<String, String> readTokenTypesFromFile(String filePath)throws Exception
+    {
+        HashMap<String, String> tokenTypes = new HashMap<>();
+        //read from file
+        return tokenTypes;
+    }
+}
