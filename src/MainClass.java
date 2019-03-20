ab89ae6e1bcc3160f5e92e71199b57b0f3407fc6
daliaalaa <daliaelhefnii@gmail.com>
2019-03-20 12:05:01 +0200

    Change main class

--- src/MainClass.java
+++ src/MainClass.java
@@ -3,6 +3,7 @@ import com.sun.xml.internal.ws.util.StringUtils;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -15,6 +16,12 @@ public class MainClass {
         return substr;
     }
 
+    static void sort(ArrayList<Token> tokens)
+    {
+        // sort tokens with respect to start_index
+        Collections.sort(tokens);
+    }
+
     public static void main (String[] Args){
         String data = null; // load data here
         try {
@@ -22,28 +29,36 @@ public class MainClass {
         } catch (Exception e) {
             e.printStackTrace();
         }
-        HashMap<String, String> tokenTypes = null; // load token_types here
+        ArrayList<TokenTypes> tokenTypes = null; // load token_types here
         try {
             tokenTypes = LoadTokenTypes.readTokenTypesFromFile("Token_Types.txt");
-        } catch (Exception e) {
+        } catch (Exception e)
+        {
             e.printStackTrace();
         }
         ArrayList<Token> tokens=new ArrayList();
         Pattern pattern;
         Matcher matcher;
-        for (String i : tokenTypes.keySet()) {
-            pattern = Pattern.compile(tokenTypes.get(i));
+        for (int i=0 ; i<tokenTypes.size();i++)
+        {
+            pattern = Pattern.compile(tokenTypes.get(i).tokenExpression);
             matcher = pattern.matcher(data);
             while (matcher.find())
             {
-                Token token = new Token(i, matcher.group(), matcher.start());
+                Token token = new Token(tokenTypes.get(i).tokenName, matcher.group(), matcher.start());
                 String substr = repeat(' ',(matcher.end() - matcher.start()));
                 String newData = data.substring(0, matcher.start())+substr+data.substring(matcher.end());
                 data = newData;
+                tokens.add(token);
             }
         }
+
+        sort(tokens); // sort tokens w.r.t start_index
+
         for (int i=0;i<tokens.size();i++){
             System.out.println(tokens.get(i).type+": "+tokens.get(i).value+"\t"+tokens.get(i).startIndex);
         }
+        String regex = "\\b[^0-9]*[a-zA-Z\\_\\$]+[0-9\\_\\$]*[a-zA-Z\\_\\$]*\\b";
+
     }
 }
