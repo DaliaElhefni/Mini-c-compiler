0f8d2ec26db4b36efddfa53f4bd4501aeff66266
daliaalaa <daliaelhefnii@gmail.com>
2019-03-20 11:55:36 +0200

    Add body to the function in LoadTokenTypes Class

--- src/LoadTokenTypes.java
+++ src/LoadTokenTypes.java
@@ -1,10 +1,24 @@
-import java.util.HashMap;
+import java.io.File;
+import java.util.ArrayList;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class LoadTokenTypes {
-    public static HashMap<String, String> readTokenTypesFromFile(String filePath)throws Exception
+    public static ArrayList<TokenTypes> readTokenTypesFromFile(String filePath)throws Exception
     {
-        HashMap<String, String> tokenTypes = new HashMap<>();
-        //read from file
+        ArrayList<TokenTypes> tokenTypes= new ArrayList<>();
+        File file = new File(filePath);
+        try (Scanner sc = new Scanner(file)) {
+            while (sc.hasNextLine())
+            {
+                String nxtline = sc.nextLine();
+                Scanner scanner = new Scanner(nxtline);
+                TokenTypes NewTT=new TokenTypes(scanner.next(), scanner.next());
+                tokenTypes.add(NewTT);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
         return tokenTypes;
     }
 }
